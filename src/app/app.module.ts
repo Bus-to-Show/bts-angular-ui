27de163b6e0f173664ec77af1f10044d568b9f01
User <user@Users-MacBook-Pro.local>
2022-04-24 02:40:17 -0600

    adding reservations and capacity to dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,17 @@ import { EventDetailComponent } from './event-detail/event-detail.component';
 import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
 import { HttpClientModule } from '@angular/common/http';
 import { EventSearchComponent } from './event-search/event-search.component';
+import { EventsTableComponent } from './events-table/events-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 
@@ -20,15 +31,27 @@ import { EventSearchComponent } from './event-search/event-search.component';
     EventsComponent,
     EventDetailComponent,
     DashboardComponent,
-    EventSearchComponent
+    EventSearchComponent,
+    EventsTableComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    
     FormsModule,
     HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
 
   ],
   providers: [],
