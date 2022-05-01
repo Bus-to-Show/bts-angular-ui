580a6e36b1780c896b769ca6cc0fdc9900e7facd
Dustin Huth <30206758+noverzero@users.noreply.github.com>
2022-04-30 18:37:03 -0600

    adding styling for pickups view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+
 
 
 
@@ -52,6 +54,7 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatCardModule,
 
   ],
   providers: [],
