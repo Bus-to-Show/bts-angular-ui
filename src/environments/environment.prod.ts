78a484e4936b496523ffb426f90f1601ff9a6425
Dustin Huth <30206758+noverzero@users.noreply.github.com>
2022-05-02 19:44:09 -0600

    fixing url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   title: 'Production',
-  API_URL: 'https://blooming-fortress-13049.herokuapp.comhttps://apiexample.com'
+  API_URL: 'https://blooming-fortress-13049.herokuapp.com'
 };
