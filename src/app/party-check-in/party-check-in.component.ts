c93064c4d2ecd1f5887b61abc2b62d9f3df875eb
Dustin Huth <30206758+noverzero@users.noreply.github.com>
2022-05-17 23:52:36 -0600

    adding filter search to party checkin and party management components.

--- src/app/party-check-in/party-check-in.component.ts
+++ src/app/party-check-in/party-check-in.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { ReservationsService } from '../services/reservations.service';
+import { Order } from '../types/order'
+
 
 
 @Component({
@@ -9,36 +11,51 @@ import { ReservationsService } from '../services/reservations.service';
   templateUrl: './party-check-in.component.html',
   styleUrls: ['./party-check-in.component.scss']
 })
+
 export class PartyCheckInComponent implements OnInit {
 partyId: number;
-reservations: [] = []
+reservations: Order[] = []
+searchTerm: string = ''
 
-  constructor(
-    private location: Location,
-    private route: ActivatedRoute,
-    private reservationsService: ReservationsService
-    ) { 
-      this.partyId = Number(this.route.snapshot.paramMap.get('id'))
+get filteredReservations(): Order[]{
+  return this.reservations.filter(reservation => {
+    return reservation.willCallFirstName.toUpperCase().includes(this.searchTerm)
+      || reservation.willCallLastName?.toUpperCase().includes(this.searchTerm)
+      || reservation.orderedByLastName?.toUpperCase().includes(this.searchTerm)
+      || reservation.orderedByFirstName?.toUpperCase().includes(this.searchTerm)
+  });
+} 
 
-    }
+constructor(
+  private location: Location,
+  private route: ActivatedRoute,
+  private reservationsService: ReservationsService
+  ) { 
+    this.partyId = Number(this.route.snapshot.paramMap.get('id'))
 
-    ngOnInit(): void {
-      this.partyId = Number(this.route.snapshot.paramMap.get('id'))
-      this.reservationsService.getReservations(this.partyId).subscribe(reservations => {
-        this.reservations = reservations
-        this.refreshReservations()
-      }); 
-    }
-    
-    refreshReservations(){
-      setInterval(()=> {
-        this.reservationsService.getReservations(this.partyId).subscribe(reservations => {
-          this.reservations = reservations
-        }); 
-      }, 20000); 
-    }
-   
-  
+}
+
+ngOnInit(): void {
+  this.partyId = Number(this.route.snapshot.paramMap.get('id'))
+  this.reservationsService.getOrders(this.partyId).subscribe(reservations => {
+    this.reservations = reservations
+    this.refreshReservations()
+  }); 
+}
+
+refreshReservations(){
+  setInterval(()=> {
+    this.reservationsService.getOrders(this.partyId).subscribe(reservations => {
+      this.reservations = reservations
+    }); 
+  }, 20000); 
+}
+//Push a search term into the observable stream.
+search(term: string): Order[] {
+  this.searchTerm = term.toUpperCase().trim();
+  return this.filteredReservations;
+}
+
   goBack(): void {
     this.location.back();
   }
--- src/app/party-management/party-management.component.ts
+++ src/app/party-management/party-management.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ReservationsService } from '../services/reservations.service';
 import { Location } from '@angular/common';
+import { Order } from '../types/order'
+
 
 @Component({
   selector: 'app-party-management',
@@ -10,7 +12,17 @@ import { Location } from '@angular/common';
 })
 export class PartyManagementComponent implements OnInit {
   partyId: number;
-  reservations: [] = []
+  reservations: Order[] = []
+  searchTerm: string = ''
+
+  get filteredReservations(): Order[]{
+    return this.reservations.filter(reservation => {
+      return reservation.willCallFirstName.toUpperCase().includes(this.searchTerm)
+        || reservation.willCallLastName?.toUpperCase().includes(this.searchTerm)
+        || reservation.orderedByLastName?.toUpperCase().includes(this.searchTerm)
+        || reservation.orderedByFirstName?.toUpperCase().includes(this.searchTerm)
+    });
+  } 
   
   constructor(
     private location: Location,
@@ -28,13 +40,19 @@ export class PartyManagementComponent implements OnInit {
       //this.refreshReservations()
     }); 
   }
-  
+
   refreshReservations(){
     setInterval(()=> {
       this.reservationsService.getOrders(this.partyId).subscribe(orders => {
         this.reservations = orders
       }); 
     }, 20000); 
+  }
+
+    //Push a search term into the observable stream.
+  search(term: string): Order[] {
+    this.searchTerm = term.toUpperCase().trim();
+    return this.filteredReservations;
   }
   
   goBack(): void {
