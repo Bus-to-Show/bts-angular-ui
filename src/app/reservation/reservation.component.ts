c062c7c6365de94cbc4230c4ea8b984550c68b22
Dave Kennedy <dave-kennedy@users.noreply.github.com>
2025-04-26 15:51:55 -0600

    Cleanup and refactor

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -1,44 +1,36 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ReservationsService } from '../services/reservations.service';
-import {Router} from '@angular/router'; // import router from angular router
+import {Component, Input} from '@angular/core';
+import {ReservationsService} from '../services/reservations.service';
+import {Order} from '../types/order';
 
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
   styleUrls: ['./reservation.component.scss']
 })
-export class ReservationComponent implements OnInit {
-  @Input() reservation: any;
-  @Input() parent!: 'check-in' | 'management'
-
+export class ReservationComponent {
+  @Input() reservation!: Order;
+  @Input() parent!: 'check-in' | 'management';
 
   constructor(
     private reservationsService: ReservationsService,
-    private route:Router
-  ) { }
-  
-  refreshPageWithRouterUntilYouGetSmartEnoughToUseABehaviorSubjectInstead(){
-      const currentUrl = this.route.url;
-      this.route.navigateByUrl('/', {skipLocationChange: true}).then(() => {
-          this.route.navigate([currentUrl]);
-      });
-  
-	//	this.route.navigate([`/party-management/${this.reservation.pickupPartiesId}`]); 
-	}
+  ) {}
 
-
-  ngOnInit(): void {
+  updateReservationStatus(changeEvent: any) {
+    this.reservationsService.updateStatus(this.reservation.id, changeEvent.checked).subscribe((response: any) => {
+      this.reservation.status = response[0].status;
+    });
   }
 
-  updateReservationStatus(changeEvent: any){
-    this.reservationsService.updateStatus(this.reservation.id, changeEvent.checked).subscribe((response: any )=> {
-    })
+  cancel() {
+    this.reservationsService.updateStatus(this.reservation.id, 'CANCEL').subscribe((response: any) => {
+      this.reservation.status = response[0].status;
+    });
   }
 
-  onCancel(makeUpYourMind: 'CANCEL' | 'UNCANCEL'){
-    //todo check availability if uncanceling
-    this.reservationsService.updateStatus(this.reservation.id, makeUpYourMind).subscribe((response: any )=> {
-      this.refreshPageWithRouterUntilYouGetSmartEnoughToUseABehaviorSubjectInstead()
-    })
+  uncancel() {
+    // TODO: check availability first
+    this.reservationsService.updateStatus(this.reservation.id, 'UNCANCEL').subscribe((response: any) => {
+      this.reservation.status = response[0].status;
+    });
   }
 }
