c7b29eb4d984ad4da80a039e82cb6a3764717a3b
Dave Kennedy <dave-kennedy@users.noreply.github.com>
2025-04-26 14:34:37 -0600

    Remove unused components and sort

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,107 +1,98 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
-import { EventsComponent } from './events/events.component';
-import { FormsModule } from '@angular/forms';
-import { EventDetailComponent } from './event-detail/event-detail.component';
-import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
-import { HttpClientModule } from '@angular/common/http';
-import { EventSearchComponent } from './event-search/event-search.component';
-import { EventsTableComponent } from './events-table/events-table.component';
-import { MatTableModule } from '@angular/material/table';
-import {MatTabsModule} from '@angular/material/tabs';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import {LayoutModule} from '@angular/cdk/layout';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import { PickupPartyComponent } from './pickup-party/pickup-party.component';
-import {MatInputModule} from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import { AddPartiesComponent } from './add-parties/add-parties.component';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatDialogModule} from '@angular/material/dialog';
-import { PartyCheckInComponent } from './party-check-in/party-check-in.component';
-import { ReservationComponent } from './reservation/reservation.component';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import { PartyManagementComponent } from './party-management/party-management.component';
-import { AddEventsComponent } from './add-events/add-events.component';
-import { AdminComponent } from './admin/admin.component';
-import { RegisterComponent } from './register/register.component';
-import { AuthComponent } from './auth/auth.component';
-import { LoginComponent } from './login/login.component';
-import { RegistrationResponseComponent } from './registration-response/registration-response.component';
-import { ResetPasswordComponent } from './reset-password/reset-password.component';
-import { ResetPassFormComponent } from './reset-pass-form/reset-pass-form.component';
-import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {JwtHelperService, JWT_OPTIONS} from '@auth0/angular-jwt';
 
+import {AddEventsComponent} from './add-events/add-events.component';
+import {AddPartiesComponent} from './add-parties/add-parties.component';
+import {AdminComponent} from './admin/admin.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AuthComponent} from './auth/auth.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {EventDetailComponent} from './event-detail/event-detail.component';
+import {EventSearchComponent} from './event-search/event-search.component';
+import {EventsComponent} from './events/events.component';
+import {LoginComponent} from './login/login.component';
+import {PartyCheckInComponent} from './party-check-in/party-check-in.component';
+import {PartyManagementComponent} from './party-management/party-management.component';
+import {PickupPartyComponent} from './pickup-party/pickup-party.component';
+import {RegisterComponent} from './register/register.component';
+import {ReservationComponent} from './reservation/reservation.component';
+import {ResetPassFormComponent} from './reset-pass-form/reset-pass-form.component';
+import {ResetPasswordComponent} from './reset-password/reset-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EventsComponent,
-    EventDetailComponent,
-    DashboardComponent,
-    EventSearchComponent,
-    EventsTableComponent,
-    NavBarComponent,
-    PickupPartyComponent,
-    AddPartiesComponent,
-    PartyCheckInComponent,
-    ReservationComponent,
-    PartyManagementComponent,
     AddEventsComponent,
+    AddPartiesComponent,
     AdminComponent,
-    RegisterComponent,
+    AppComponent,
     AuthComponent,
+    DashboardComponent,
+    EventDetailComponent,
+    EventSearchComponent,
+    EventsComponent,
     LoginComponent,
-    RegistrationResponseComponent,
+    PartyCheckInComponent,
+    PartyManagementComponent,
+    PickupPartyComponent,
+    RegisterComponent,
+    ReservationComponent,
+    ResetPassFormComponent,
     ResetPasswordComponent,
-    ResetPassFormComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
+    BrowserModule,
     FormsModule,
     HttpClientModule,
-    MatTableModule,
-    MatTabsModule,
-    MatPaginatorModule,
-    MatSortModule,
     LayoutModule,
-    MatToolbarModule,
     MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
     MatCardModule,
-    MatProgressBarModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    MatSnackBarModule,
     MatCheckboxModule,
     MatDialogModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
     MatSlideToggleModule,
-    MatFormFieldModule
-    
-
+    MatSliderModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
   ],
-  providers: [ { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
-    JwtHelperService],
+  providers: [
+    {provide: JWT_OPTIONS, useValue: JWT_OPTIONS},
+    JwtHelperService,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
